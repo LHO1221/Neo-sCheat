8700dec1b27103830f01dcb831ab2532d37e7ca2
LHO1221 <eun3321kim@gmail.com>
2025-12-11 12:02:34 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -14,7 +14,7 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
-ScreenGui.DisplayOrder = 10
+--ScreenGui.DisplayOrder = 10
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
 ScreenGui.Parent = CoreGui;
 
